e1cf69fe749371e0d38cf49ff209c27ddbfc7ce3
Colin Jaffe <balloonasaurus@gmail.com>
2019-11-18 12:14:47 -0500

    Initial commit.

---
+++ main.js
@@ -0,0 +1,57 @@
+function getFirstName(person) {
+  return person.firstName;
+}
+
+function getLastName(person) {
+  return person.lastName;
+}
+
+function getFullName(person) {
+  return `${person.firstName} ${person.lastName}`
+}
+
+function setFirstName(person, name) {
+  person.firstName = name;
+}
+
+function setAge(person, age) {
+  person.age = age;
+}
+
+function giveBirthday(person) {
+  if (person.age) {
+    person.age++;
+  } else {
+    person.age = 1;
+  }
+
+  // Alternate common JavaScript shorthand:
+  // person.age = person.age + 1 || 1;
+}
+
+function marry(person1, person2) {
+  person1.married = true;
+  person2.married = true;
+
+  person1.spouseName = getFullName(person2);
+  person2.spouseName = getFullName(person1);
+}
+
+function divorce(person1, person2) {
+  person1.married = false;
+  person2.married = false;
+
+  delete person1.spouseName
+  delete person2.spouseName
+}
+
+module.exports = {
+  getFirstName,
+  getLastName,
+  getFullName,
+  setFirstName,
+  setAge,
+  giveBirthday,
+  marry,
+  divorce,
+} 
